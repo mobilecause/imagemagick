9c50848ce2afd7a45975dfa62d0cfd70ce1ed630
Santhosh Ramaiah <santhosh.r411@gmail.com>
2025-08-12 18:32:36 +0530

    fix install script

--- imagemagick-repo/client-setup/install.sh
+++ imagemagick-repo/client-setup/install.sh
@@ -6,7 +6,7 @@ REPO_URL="https://raw.githubusercontent.com/mobilecause/imagemagick/main"
 echo "Installing ImageMagick repository..."
 
 # Download repo config with priority=9
-curl -fsSL "$REPO_URL/imagemagick-repo/client-setup/imagemagick-build.repo" \\
+curl -fsSL "$REPO_URL/imagemagick-repo/client-setup/imagemagick-build.repo" \
     -o "/etc/yum.repos.d/imagemagick-build.repo"
 
 # Refresh cache
